9984a006446ef83e0ca36e9d29e1a8d518982f5a
Benjamin Simon <benjh.simon@gmail.com>
2025-07-03 19:02:08 +0200

    add terraform-local version when using version flag

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -674,6 +674,23 @@ def check_override_file_content_for_alias(override_file):
     return True
 
 
+@pytest.mark.parametrize("version_flag", ["--version", "-v", "-version"])
+def test_version_command(monkeypatch, version_flag):
+    def _run(cmd, **kwargs):
+        kwargs["stderr"] = subprocess.STDOUT
+        return subprocess.check_output(cmd, **kwargs)
+
+    with tempfile.TemporaryDirectory(delete=True) as temp_dir:
+        # we need the `terraform init` command to create a lock file, so it cannot be a `DRY_RUN`
+        output = _run([TFLOCAL_BIN, version_flag], cwd=temp_dir, env=dict(os.environ))
+        assert b"terraform-local v" in output
+
+        monkeypatch.setenv("DRY_RUN", "1")
+        output = _run([TFLOCAL_BIN, version_flag], cwd=temp_dir, env=dict(os.environ))
+
+        assert b"terraform-local v" in output
+
+
 ###
 # UTIL FUNCTIONS
 ###
